f14c1439e3d35620766f9be6e8dd9f5d2671ac11
320dollars <44992926+320dollars@users.noreply.github.com>
2018-12-12 16:46:50 +0300

    final

--- js/scripts.js
+++ js/scripts.js
@@ -4,18 +4,32 @@ var exitPopup = document.querySelector(".exit");
 var buttonMap = document.querySelector(".open-map");
 var map = document.querySelector(".map");
 var exitMap = document.querySelector(".exit-map");
-var tovat = document.querySelectorAll(".tovat");
-var imgTovat = document.querySelectorAll(".tvt");
+var login = popup.querySelector("input[name=name]");
+var form = popup.querySelector("form");
+var email = popup.querySelector("input[name=e-mail]");
+var text = popup.querySelector("textarea");
 
+var isStorageSupport = true;
+var storage = "";
 
-var slaider = document.querySelector(".slaider");
-var buttonNext = document.querySelector(".next");
-var buttonPrevious = document.querySelector(".previous");
+try {
+  storage.localStorage.getItem("login");
+} catch (err) {
+  isStorageSupport = false;
+}
+
+if (storage) {
+  login.value = storage;
+  email.focus();
+} else {
+  login.focus();
+}
 
 function openThat(nameObj, modalWin, className) {
   nameObj.addEventListener("click", function(evt) {
     evt.preventDefault();
     modalWin.classList.add(className);
+    login.focus();
   });
 }
 
@@ -23,6 +37,28 @@ function closeThat(nameObj, modalWin, className) {
   nameObj.addEventListener("click", function(evt) {
     evt.preventDefault();
     modalWin.classList.remove(className);
+    popup.classList.remove("modal-error");
+  });
+}
+
+form.addEventListener("submit", function (evt) {
+  evt.preventDefault();
+  if (!login.value || !email.value || !text.value) {
+    popup.classList.add("modal-error");
+  } else {
+    localStorage.setItem("login", login.value);
+  }
+});
+
+function downEsc(nameObj, className) {
+  window.addEventListener("keydown", function(evt) {
+    if (evt.keyCode === 27) {
+      evt.preventDefault();
+
+      if (nameObj.classList.contains(className)) {
+        nameObj.classList.remove(className);
+      }
+    }
   });
 }
 
@@ -30,9 +66,5 @@ openThat(buttonPopup, popup, "write-us-show");
 openThat(buttonMap, map, "map-show");
 closeThat(exitPopup, popup, "write-us-show");
 closeThat(exitMap, map, "map-show");
-
-for (var i = 0; i < tovat.length; i++) {
- tovat[i].addEventListener("mouseover", function(){
-   imgTovat[j].classList.add("hidden-img");
- });
-}
+downEsc(popup, "write-us-show");
+downEsc(map, "map-show");
